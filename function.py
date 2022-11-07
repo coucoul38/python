643a0a5472956b6c7605a76b66fd69be550c9cf9
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-11-07 11:24:46 +0100

    exo fonctions

---
+++ function.py
@@ -0,0 +1,20 @@
+def add(x, y):
+    return(x+y)
+
+def sub(x,y):
+    return(x-y)
+
+def multiply(x, y):
+    return(x*y)
+
+def divide(x,y):
+    return(x/y)
+
+def modulo(x,y):
+    return(x%y)
+
+def calcSalaireSecParAn(salaireH, heureParJourOuvrable, JOparAn):
+    return((salaireH*heureParJourOuvrable)*5/7)
+
+def calcNet(salaireBrut):
+    return(salaireNet)
