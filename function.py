e747ac0139f0d13f3cea3ac7858cc9e3a15f4f6a
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-11-08 09:10:20 +0100

    division par 0 fix

--- function.py
+++ function.py
@@ -8,7 +8,10 @@ def multiply(x, y):
     return(x*y)
 
 def divide(x,y):
-    return(x/y)
+    # empecher la division par 0
+    if(y == 0):
+        return('Y ne peux pas être égal à 0')
+    return(x/y)  # éxécuté seulement si y n'est pas 0
 
 def modulo(x,y):
     return(x%y)
@@ -23,5 +26,6 @@ def calcSalaireSec(salaireH, heureJourOuvrable, JOparAn):
 def calcNet(salaireBrut, fonctionPublique):
     if fonctionPublique :
         return(salaireBrut-(0.15*salaireBrut)) # 15% de taxes pour le public
+    # Si pas dans la fonction publique, alors taxes privé
     else :
         return(salaireBrut-(0.23*salaireBrut))  # 23% de taxes pour le reste
