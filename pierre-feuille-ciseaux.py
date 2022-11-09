50f14080c57762cac5fb1026a80b432e84bd5cc7
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-11-09 14:16:12 +0100

    pierre feuille ciseaux v0

---
+++ pierre-feuille-ciseaux.py
@@ -0,0 +1,23 @@
+#v0
+
+#le joueur entre son choix (p,f,s)
+    #0 = pierre
+    #1 = feuille
+    #2 = ciseaux
+    #on admet une fonction random()
+    #on choisi un résultat aléatoir entre p,f,c: ce que va jouer le bot
+
+    #si le joueur à joué pierre
+        #si le bot à joué ciseau, victoire du joueur
+        #se le bot à joué feuille, le joueur perds
+        #sinon égalité
+    
+    #si le joueur à joué feuille
+        #si le bot à joué ciseaux, victoire du bot
+        #si le bot à joué pierre, victoire du joueur
+        #sinon égalité
+    
+    #si le joueur à joué ciseaux
+        #si le bot à joué pierre, victoire du bot
+        #si le bot à joué feuille, victoire du joueur
+        #sinon égalité
