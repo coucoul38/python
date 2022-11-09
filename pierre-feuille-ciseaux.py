0263b8f0225f415fef71ce727ff5dca6ffba03a2
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-11-09 14:44:25 +0100

    pfc v0.3.1

--- pierre-feuille-ciseaux.py
+++ pierre-feuille-ciseaux.py
@@ -1,4 +1,4 @@
-#v0.3
+#v0.3.1
 #on admet une fonction random() qui permet de choisir une option aléatoirement
 #on admet une fonction input(key) qui permet de récupérer l'entrée d'un joueur
 
@@ -6,35 +6,36 @@
 
 #fontion end() : finit la partie (manche=nombreDeManches)
 
-#tant que manche <= nombreDeManches
-    #le joueur entre son choix
-        #1 = pierre
-        #2 = feuille
-        #3 = ciseaux
-        
-    #on choisis un résultat aléatoire entre 1,2,3: ce que va jouer le bot
+#foncion du jeu (play())
+    #tant que manche <= nombreDeManches
+        #le joueur entre son choix
+            #1 = pierre
+            #2 = feuille
+            #3 = ciseaux
 
-    #si le joueur à joué pareil que le bot
-        #dire qu'il y a eu égalité
+        #on choisis un résultat aléatoire entre 1,2,3: ce que va jouer le bot
 
-    #si le joueur à joué pierre
-        #si le bot à joué ciseau, victoire du joueur
-            #+1 point au joueur
-        #se le bot à joué feuille, le joueur perds
-            #+1 point au bot
+        #si le joueur à joué pareil que le bot
+            #dire qu'il y a eu égalité
 
-    #si le joueur à joué feuille
-        #si le bot à joué ciseaux, victoire du bot
-            #+1 point au bot
-        #si le bot à joué pierre, victoire du joueur
-            #+1 point au joueur
+        #si le joueur à joué pierre
+            #si le bot à joué ciseau, victoire du joueur
+                #+1 point au joueur
+            #se le bot à joué feuille, le joueur perds
+                #+1 point au bot
 
-    #si le joueur à joué ciseaux
-        #si le bot à joué pierre, victoire du bot
-            #+1 point au bot
-        #si le bot à joué feuille, victoire du joueur
-            #+1 point au joueur
+        #si le joueur à joué feuille
+            #si le bot à joué ciseaux, victoire du bot
+                #+1 point au bot
+            #si le bot à joué pierre, victoire du joueur
+                #+1 point au joueur
 
-    #manche = manche + 1
-    #donner le score total
+        #si le joueur à joué ciseaux
+            #si le bot à joué pierre, victoire du bot
+                #+1 point au bot
+            #si le bot à joué feuille, victoire du joueur
+                #+1 point au joueur
+
+        #manche = manche + 1
+        #donner le score total
 
