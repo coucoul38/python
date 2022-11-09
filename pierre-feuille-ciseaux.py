d627dd7dd152f22c7bc9e39b04d71cbfd56efc99
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-11-09 15:03:29 +0100

    pfc v0.4

--- pierre-feuille-ciseaux.py
+++ pierre-feuille-ciseaux.py
@@ -1,42 +1,52 @@
-#v0.3.2
+#v0.4
+
 #on admet une fonction random() qui permet de choisir une option aléatoirement
 #on admet une fonction input() qui permet de récupérer l'entrée d'un joueur
 
-#le joueur entre le nombre de manches
 
 #fontion end() : finit la partie (manche=nombreDeManches)
 
 #foncion du jeu (play())
+#on récupère le nombre de manche grace à input() et on l'assigne à nombreDeManches
     #tant que manche <= nombreDeManches
-        #le joueur entre son choix
-            #1 = pierre
-            #2 = feuille
-            #3 = ciseaux
+        #le joueur entre son choix grace à input() : variable choix
+            #si choix = p
+                #alors choixNb = 1
+            #si choix = f
+                #alors choixNb = 2
+            #si choix = c
+                #alors choixNb = 3
 
-        #on choisis un résultat aléatoire entre 1,2,3: ce que va jouer le bot
+        #on génère un résultat aléatoire entre 1,2,3 avec la fonction random() et on l'assigne à choixBot
 
-        #si le joueur à joué pareil que le bot
-            #dire qu'il y a eu égalité
+        #si choixNb = choixBot
+            #alors dire qu'il y a eu égalité
 
-        #sinon si le joueur à joué pierre
-            #si le bot à joué ciseau, victoire du joueur
-                #+1 point au joueur
-            #se le bot à joué feuille, le joueur perds
-                #+1 point au bot
+        #sinon si choixNb = 1
+            #alors si choixBot = 2
+                #dire que le joueur à perdu
+                #ajouter 1 au score du bot
+            #alors si choixBot = 3
+                #dire que le joueur à gagné
+                #ajouter 1 au score du joueur
 
-        #sinon si le joueur à joué feuille
-            #si le bot à joué ciseaux, victoire du bot
-                #+1 point au bot
-            #si le bot à joué pierre, victoire du joueur
-                #+1 point au joueur
+        #sinon si choixNb = 2
+            #alors si choixBot = 1
+                #dire que le joueur à gangé
+                #ajouter 1 au score du joueur
+            #alors si choixBot = 3
+                #dire que le joueur à perdu
+                #ajouter 1 au score du bot
 
-        #sinon si le joueur à joué ciseaux
-            #si le bot à joué pierre, victoire du bot
-                #+1 point au bot
-            #si le bot à joué feuille, victoire du joueur
-                #+1 point au joueur
+        #sinon si choixNb = 3
+            #alors si choixBot = 1
+                #dire que le joueur à perdu
+                #ajouter 1 au score du bot
+            #alors si choixBot = 2
+                #dire que le joueur à gagné
+                #ajouter 1 au score du joueur
 
-        #manche = manche + 1
+        #ajouter 1 à manche
         #donner le score total
     
     #après toutes les manches, donner le gagnant
