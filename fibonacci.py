de2a742426be7ff733c8dd8b5ed1b7a11e2da703
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-11-09 15:21:06 +0100

    fibo commentaires

--- fibonacci.py
+++ fibonacci.py
@@ -2,10 +2,13 @@
 
 
 def fibo(iterations):
+    #on initialise les 1ers chiffres
     n1, n2 = 0, 1
+    #on execute le nombre de fois demandé
     for i in range(iterations):
         n3 = n2 + n1
         print(n1)
         n1 = n2
         n2 = n3
-fibo(7)    
+
+fibo(69)    
