9277cbb4fb812fa9d9e276c9bd76fe48455e9549
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-11-10 18:29:09 +0100

    display

---
+++ game of life.py
@@ -0,0 +1,37 @@
+#tableau tabA len(x)
+#dans tabA, nombre x de tableaux de longueur x
+
+#tabA[0][1] --> valeur
+#ensuite retourner toutes les cases adjacentes 
+
+import random
+tabA=[]
+def createGrid(x):
+    #initialisation de la liste contenant chaque colonne
+    
+    #on crée x lignes
+    for i in range(x):
+        #on crée une ligne de longueur x 
+        tabB=[]
+        for o in range(x):
+            #on met un nombre aléatoire dans chaque case
+            tabB.append(random.randint(0,9))
+        #on insère la ligne aux colonnes
+        tabA.append(tabB)
+    
+    for p in range(len(tabA)):
+        print(tabA[p])
+
+def showTouchingCoordinates(column, row):
+    display = [
+        [tabA[column-1][row-1],tabA[column-1][row],tabA[column-1][row+1]],
+        [tabA[column][row-1],tabA[column][row], tabA[column][row+1]],
+        [tabA[column+1][row-1],tabA[column+1][row],tabA[column+1][row+1]]
+    ]
+    for p in range(len(display)):
+        print(display[p])
+
+
+createGrid(10)
+print(tabA[1][1])
+showTouchingCoordinates(1,1)
