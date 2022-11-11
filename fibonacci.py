51eea87ec8f3f298a48218a2b88f3bc51713255f
coucoul38 <44196664+coucoul38@users.noreply.github.com>
2022-11-11 16:03:36 +0100

    fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -10,14 +10,3 @@ def fibo(iterations, n2):
         n2 = n3
 
 fibo(69,3)
-
-
-#marche pas
-def fiboList(iterations):
-    #on initialise les 1ers chiffres
-    list=[0,1]
-    #on execute le nombre de fois demandé
-    for i in range(iterations):
-        list.append(list[i-1] + list[i])
-        i=i+1
-    print(list)
